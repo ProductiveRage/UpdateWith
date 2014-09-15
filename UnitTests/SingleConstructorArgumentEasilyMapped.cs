ffa694a508b772426a040acb1afbd0930f4cfc73
dan <dan@newmind.co.uk>
2014-09-15 22:11:57 +0100

    Added UnitTests framework and two simple demonstration tests

---
+++ UnitTests/SingleConstructorArgumentEasilyMapped.cs
@@ -0,0 +1,19 @@
+﻿using UpdateWithExamples;
+
+namespace UnitTests
+{
+	public class SingleConstructorArgumentEasilyMapped
+	{
+		public SingleConstructorArgumentEasilyMapped(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get; private set; }
+
+		public SingleConstructorArgumentEasilyMapped UpdateWith(OptionalValue<string> name = new OptionalValue<string>())
+		{
+			return DefaultUpdateWithHelper.GetGenerator<SingleConstructorArgumentEasilyMapped>()(this, name);
+		}
+	}
+}
