def3d50a59217c06a3c5712444e897a9676c31be
dan <dan@newmind.co.uk>
2014-09-15 22:36:28 +0100

    More unit tests - one to illustrate that null values may be provided as update argument values and a couple to illustrate some runtime error cases

--- UnitTests/DefaultUpdateWithHelperTests.cs
+++ UnitTests/DefaultUpdateWithHelperTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Xunit;
 
 namespace UnitTests
@@ -52,6 +53,34 @@ namespace UnitTests
 			Assert.Equal("test-new", newInstance.Name);
 			var privateIdField = newInstance.GetType().GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
 			Assert.Equal(-1, privateIdField.GetValue(newInstance));
+		}
+
+		[Fact]
+		public void NullValuesMayBeSpecifiedAndUpdateWithExtensionMethodsAreSupported()
+		{
+			var currentInstance = new RoleDetails("test", new DateTime(2014, 9, 15), new DateTime(2014, 10, 16));
+			var newInstance = currentInstance.UpdateWith(title: "test-new", endDateIfAny: null);
+			Assert.Equal(null, newInstance.EndDateIfAny);
+		}
+
+		[Fact]
+		public void UnmappableUpdateArgumentsResultInAnArgumentException()
+		{
+			var currentInstance = new FaultyNamedType("test");
+			Assert.Throws<ArgumentException>(() =>
+			{
+				Console.WriteLine(currentInstance.UpdateWith(title: "test-new").Name);
+			});
+		}
+
+		[Fact]
+		public void UnmatchedNumberOfUpdateArgumentsPassedInsideTheUpdateWithMethodResultInAnArgumentException()
+		{
+			var currentInstance = new FaultyNamedAndNumberedType("test", 1);
+			Assert.Throws<ArgumentException>(() =>
+			{
+				Console.WriteLine(currentInstance.UpdateWith(name: "test-new").Name);
+			});
 		}
 	}
 }
---
+++ UnitTests/FaultyNamedAndNumberedType.cs
@@ -0,0 +1,25 @@
+﻿using UpdateWithExamples;
+
+namespace UnitTests
+{
+	/// <summary>
+	/// The UpdateWith method is faulty on this class since the method itself only has a single update argument but it passes two arguments to
+	/// the GetGenerator result
+	/// </summary>
+	public class FaultyNamedAndNumberedType
+	{
+		public FaultyNamedAndNumberedType(string name, int id)
+		{
+			Name = name;
+			Id = id;
+		}
+
+		public string Name { get; private set; }
+		public int Id { get; private set; }
+
+		public FaultyNamedAndNumberedType UpdateWith(OptionalValue<string> name = new OptionalValue<string>())
+		{
+			return DefaultUpdateWithHelper.GetGenerator<FaultyNamedAndNumberedType>()(this, name, 12);
+		}
+	}
+}
