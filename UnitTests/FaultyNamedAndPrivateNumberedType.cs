6ae808532de5dc3755cfd009bc85e84648fa66f4
dan <dan@newmind.co.uk>
2014-09-15 22:42:12 +0100

    Another error case - where there is a constructor argument that can not be satisfied

---
+++ UnitTests/FaultyNamedAndPrivateNumberedType.cs
@@ -0,0 +1,24 @@
+﻿using UpdateWithExamples;
+
+namespace UnitTests
+{
+	/// <summary>
+	/// This class' UpdateWith fails because there is no way for it to provide a value for the constructor's id argument
+	/// </summary>
+	public class FaultyNamedAndPrivateNumberedType
+	{
+		private readonly int _id;
+		public FaultyNamedAndPrivateNumberedType(string name, int id)
+		{
+			Name = name;
+			_id = id;
+		}
+
+		public string Name { get; private set; }
+
+		public FaultyNamedAndPrivateNumberedType UpdateWith(OptionalValue<string> name = new OptionalValue<string>())
+		{
+			return DefaultUpdateWithHelper.GetGenerator<FaultyNamedAndPrivateNumberedType>()(this, name);
+		}
+	}
+}
