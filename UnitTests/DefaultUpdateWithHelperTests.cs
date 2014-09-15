45f6c2cea3263a530fad45f0d8d4827ad047c534
dan <dan@newmind.co.uk>
2014-09-15 22:24:18 +0100

    Added some more basic test cases

--- UnitTests/DefaultUpdateWithHelperTests.cs
+++ UnitTests/DefaultUpdateWithHelperTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Reflection;
+using Xunit;
 
 namespace UnitTests
 {
@@ -26,6 +27,31 @@ namespace UnitTests
 			var currentInstance = new NamedType("test");
 			var newInstance = currentInstance.UpdateWith();
 			Assert.Equal(currentInstance, newInstance);
+		}
+
+		/// <summary>
+		/// There is no update argument on NamedAndNumberedType for id, so the value must be maintained by taking it from the source reference
+		/// </summary>
+		[Fact]
+		public void PropertyValueFallbackDueToUnavailableUpdateArgument()
+		{
+			var currentInstance = new NamedAndNumberedType("test", 1);
+			var newInstance = currentInstance.UpdateWith(name: "test-new");
+			Assert.Equal("test-new", newInstance.Name);
+			Assert.Equal(1, newInstance.Id);
+		}
+
+		/// <summary>
+		/// There is no update argument on NamedAndNumberedType for id, so the value must be maintained by taking it from the source reference
+		/// </summary>
+		[Fact]
+		public void ConstructorArgumentDefaultValueFallbackDueToUnavailableUpdateArgument()
+		{
+			var currentInstance = new NamedAndPrivateNumberedType("test", 1);
+			var newInstance = currentInstance.UpdateWith(name: "test-new");
+			Assert.Equal("test-new", newInstance.Name);
+			var privateIdField = newInstance.GetType().GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
+			Assert.Equal(-1, privateIdField.GetValue(newInstance));
 		}
 	}
 }
---
+++ UnitTests/NamedAndPrivateNumberedType.cs
@@ -0,0 +1,26 @@
+﻿using UpdateWithExamples;
+
+namespace UnitTests
+{
+	/// <summary>
+	/// This is still a very simple type but one for which one property is not available in either the UpdateWith method or through any public
+	/// properties. The only way it can be satisfied is by falling back to the constructor argument's default value.
+	/// by the property value on the source reference
+	/// </summary>
+	public class NamedAndPrivateNumberedType
+	{
+		private readonly int _id;
+		public NamedAndPrivateNumberedType(string name, int id = -1)
+		{
+			Name = name;
+			_id = id;
+		}
+
+		public string Name { get; private set; }
+
+		public NamedAndPrivateNumberedType UpdateWith(OptionalValue<string> name = new OptionalValue<string>())
+		{
+			return DefaultUpdateWithHelper.GetGenerator<NamedAndPrivateNumberedType>()(this, name);
+		}
+	}
+}
