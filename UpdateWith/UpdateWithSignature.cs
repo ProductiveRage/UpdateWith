a95049a6f4464f8b2255bcaa090954a5e55d7c0f
dan <dan@newmind.co.uk>
2014-09-16 21:18:48 +0100

    Added "GetUncachedGenerator" methods to the classes, which require methods with a fixed number of arguments but avoid requiring allocating an array of argument value updates. A trade-off in convenience for possibly a very smaller performance improvement via one less GC allocation (if I'm being completely honest, possibly a bit silly if any arguments are value types since they will have to be boxed into objects and if there are more than one then it will overshadow this optimisation)

--- UpdateWith/UpdateWithSignature.cs
+++ UpdateWith/UpdateWithSignature.cs
@@ -1,4 +1,14 @@
 ﻿namespace UpdateWithExamples
 {
 	public delegate T UpdateWithSignature<T>(T source, params object[] updateValues);
+
+	public delegate T UpdateWithSignature1<T>(T source, object arg0);
+	public delegate T UpdateWithSignature2<T>(T source, object arg0, object arg1);
+	public delegate T UpdateWithSignature3<T>(T source, object arg0, object arg1, object arg2);
+	public delegate T UpdateWithSignature4<T>(T source, object arg0, object arg1, object arg2, object arg3);
+	public delegate T UpdateWithSignature5<T>(T source, object arg0, object arg1, object arg2, object arg3, object arg4);
+	public delegate T UpdateWithSignature6<T>(T source, object arg0, object arg1, object arg2, object arg3, object arg4, object arg5);
+	public delegate T UpdateWithSignature7<T>(T source, object arg0, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6);
+	public delegate T UpdateWithSignature8<T>(T source, object arg0, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7);
+	public delegate T UpdateWithSignature9<T>(T source, object arg0, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8);
 }
